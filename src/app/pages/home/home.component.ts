1465eb65c07cc1fce137bb2fdb0ec0cf89dc8697
Abhishek Kumar <kumar.abhishek@vfirst.com>
2020-08-17 18:18:50 +0530

    mugulix implemented

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,23 +3,21 @@ import { OwlOptions, SlidesOutputData } from 'ngx-owl-carousel-o';
 import { HomeService } from './home.service';
 import { newArray } from '@angular/compiler/src/util';
 
-
 @Component({
-    selector:'app-home',
-    templateUrl:'./home.component.html',
-    styleUrls:['./home.component.scss']
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
 })
+export class HomeComponent {
+  @ViewChild('homeCrowsel', { static: true }) homeCrowsel;
+  moglixHomePageData: any;
+  categories: Array<any>;
+  advertisers: Array<any>;
+  flayersImages: Array<any>;
+  dataForActiveCarousel = this.flayersImages;
+  activeSlidesCaption: Array<Boolean>;
 
-export class  HomeComponent{
-    @ViewChild('homeCrowsel', {static: true}) homeCrowsel;
-    moglixHomePageData: any;
-    categories:Array<any>;
-    advertisers:Array<any>;
-    flayersImages:Array<any>;
-    dataForActiveCarousel=this.flayersImages;
-    activeSlidesCaption:Array<Boolean>;
-    
-customOptions: OwlOptions = {
+  customOptions: OwlOptions = {
     loop: true,
     mouseDrag: true,
     touchDrag: true,
@@ -29,27 +27,27 @@ customOptions: OwlOptions = {
     autoplay: true,
     autoplaySpeed: 1000,
     responsive: {
-        0: {
-        items: 1
-        },
-        400: {
-        items: 1
-        },
-        740: {
-        items: 1
-        },
-        940: {
-        items: 1
-        }
+      0: {
+        items: 1,
+      },
+      400: {
+        items: 1,
+      },
+      740: {
+        items: 1,
+      },
+      940: {
+        items: 1,
+      },
     },
     nav: true,
-    navText: [  '<i class="fa fa-chevron-left" aria-hidden="true"></i>',
-                '<i class="fa fa-chevron-right" aria-hidden="true"></i>']
-    }
+    navText: [
+      '<i class="fa fa-chevron-left" aria-hidden="true"></i>',
+      '<i class="fa fa-chevron-right" aria-hidden="true"></i>',
+    ],
+  };
 
-
-
-productSlideOption: OwlOptions = {
+  productSlideOption: OwlOptions = {
     loop: false,
     mouseDrag: true,
     touchDrag: true,
@@ -57,69 +55,68 @@ productSlideOption: OwlOptions = {
     dots: false,
     stagePadding: 0,
     navSpeed: 700,
-   margin: 0,
+    margin: 0,
+    autoHeight: false,
     responsive: {
-        0: {
-        items: 1
-        },
-        400: {
+      0: {
         items: 1,
-        },
-        740: {
+      },
+      400: {
+        items: 1,
+      },
+      740: {
         items: 6,
-        },
-        940: {
+      },
+      940: {
         items: 6,
-        }
+      },
     },
     nav: true,
-    navText: [  '<i class="fa fa-arrow-left" aria-hidden="true"></i>',
-                '<i class="fa fa-arrow-right" aria-hidden="true"></i>' ]
-    }
+    navText: [
+      '<i class="fa fa-arrow-left" aria-hidden="true"></i>',
+      '<i class="fa fa-arrow-right" aria-hidden="true"></i>',
+    ],
+  };
 
-    constructor(private _homeService: HomeService) {
-        this.activeSlidesCaption=newArray(5).fill(false);
-        
-    }
-    
+  constructor(private _homeService: HomeService) {
+    this.activeSlidesCaption = newArray(5).fill(false);
+  }
 
-    ngOnInit() {
-        const data = {
-            "category_list": [
-            "5f279f68e51fa82349684923",
-            "5f279f6ce51fa82349684924",
-            "5f279f7fe51fa82349684925",
-            "5f279f8de51fa82349684926",
-            "5f279f9fe51fa82349684927",
-            "5f279fa4e51fa82349684928",
-            "5f297538c25c696b14ef3019",
-            "5f297373c25c696b14ef3015"
-            ]};
-        this._homeService.getCategoryProducts(data).subscribe(res => {
-            
-            this.moglixHomePageData=res;
-            console.log(this.moglixHomePageData);
-            this.categories = this.moglixHomePageData.data.category_data;
-            console.log(this.categories );
-            this.advertisers = this.moglixHomePageData.data.banner_data.advertisement;
-            this.flayersImages = this.moglixHomePageData.data.banner_data.flyer;
-        });
-    }
+  ngOnInit() {
+    const data = {
+      category_list: [
+        '5f279f68e51fa82349684923',
+        '5f279f6ce51fa82349684924',
+        '5f279f7fe51fa82349684925',
+        '5f279f8de51fa82349684926',
+        '5f279f9fe51fa82349684927',
+        '5f279fa4e51fa82349684928',
+        '5f297538c25c696b14ef3019',
+        '5f297373c25c696b14ef3015',
+      ],
+    };
+    this._homeService.getCategoryProducts(data).subscribe((res) => {
+      this.moglixHomePageData = res;
+      console.log(this.moglixHomePageData);
+      this.categories = this.moglixHomePageData.data.category_data;
+      console.log(this.categories);
+      this.advertisers = this.moglixHomePageData.data.banner_data.advertisement;
+      this.flayersImages = this.moglixHomePageData.data.banner_data.flyer;
+    });
+  }
 
-    goToSlide(index){
-      this.homeCrowsel.to('' + index);
-      console.log(this.homeCrowsel );
-    }
+  goToSlide(index) {
+    this.homeCrowsel.to('' + index);
+    console.log(this.homeCrowsel);
+  }
 
-    activeSlides: SlidesOutputData;
-    getData(dataForActiveCarousel: SlidesOutputData) {
-      this.activeSlides = dataForActiveCarousel;
-      console.log(this.activeSlides);
-      setTimeout(()=>{
-      this.activeSlidesCaption=newArray(5).fill(false);
-      this.activeSlidesCaption[dataForActiveCarousel.startPosition]=true;
-      
-      },0);
-    }
-    
+  activeSlides: SlidesOutputData;
+  getData(dataForActiveCarousel: SlidesOutputData) {
+    this.activeSlides = dataForActiveCarousel;
+    console.log(this.activeSlides);
+    setTimeout(() => {
+      this.activeSlidesCaption = newArray(5).fill(false);
+      this.activeSlidesCaption[dataForActiveCarousel.startPosition] = true;
+    }, 0);
+  }
 }
