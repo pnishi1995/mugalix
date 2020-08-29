e14ae9ff9b509dd1457431e0941c958ba1db6ccd
Abhishek Kumar <kumar.abhishek@vfirst.com>
2020-08-29 18:18:07 +0530

    build made and pushed

--- src/app/pages/categories/sub-categories/sub-categories.component.ts
+++ src/app/pages/categories/sub-categories/sub-categories.component.ts
@@ -1,32 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { SubCategoriesService } from "./sub-categories.service";
+import { SubCategoriesService } from './sub-categories.service';
 @Component({
   selector: 'app-sub-categories',
   templateUrl: './sub-categories.component.html',
-  styleUrls: ['./sub-categories.component.scss']
+  styleUrls: ['./sub-categories.component.scss'],
 })
 export class SubCategoriesComponent implements OnInit {
-  productList:any;
+  productList: any;
 
-  constructor(public _routes:ActivatedRoute, 
-  public _subCategoriesService:SubCategoriesService) { 
-  
-  }
-
+  constructor(
+    public _routes: ActivatedRoute,
+    public _subCategoriesService: SubCategoriesService
+  ) {}
 
   ngOnInit(): void {
     this.getProductListAbhi();
   }
 
-  getProductListAbhi(){
+  page: number = 1;
+  limit: number = 15;
+  getProductListAbhi() {
     let param = '';
-    param += this._routes.params['value'].categoryId ? '?category=' + this._routes.params['value'].categoryId : '';
-    param += this._routes.params['value'].subcategoryId ? '&sub_category=' + this._routes.params['value'].subcategoryId : '';
+    param += this._routes.params['value'].categoryId
+      ? '?category=' + this._routes.params['value'].categoryId
+      : '';
+    param += this._routes.params['value'].subcategoryId
+      ? '&sub_category=' + this._routes.params['value'].subcategoryId
+      : '';
 
-    this._subCategoriesService.getProductList(param).subscribe(res => {
+    param += '&page=' + this.page + '&limit=' + this.limit;
+
+    this._subCategoriesService.getProductList(param).subscribe((res) => {
       if (res['status']) {
-        this.productList = res['data'];
+        this.productList = res['data']['results'];
       }
     });
   }
